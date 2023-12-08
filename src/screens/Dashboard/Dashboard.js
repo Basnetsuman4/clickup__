6808817d02f81e5f33a0bd85e2ccec240cfe4fc5
Suman Basnet <Arunbasnet54@gmail.com>
2023-12-08 10:35:13 +0545

    Dashboard Component

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -3,10 +3,20 @@ import { ReactComponent as Option } from '../../assets/icons/options-horizontal.
 import Logo from '../../assets/images/logo.svg'
 import { ReactComponent as Todo } from '../../assets/icons/todo.svg'
 import { ReactComponent as Form } from '../../assets/icons/form.svg'
-
+// import ToDo from '../../components/DnD'
+import { NavLink, Outlet } from 'react-router-dom'
 
 
 const Dashboard = () => {
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'none',
+            color: isActive ? ' aliceblue' : 'aliceblue',
+        }
+    }
+
+
     return (
         <div className="Dashboard">
             <div className="header">
@@ -15,24 +25,9 @@ const Dashboard = () => {
                         <img src={Logo} alt='logo' />
                         <div className="title">Widget Site</div>
                     </div>
-                    <div className="optionBtn">
-                        <Option />
-                    </div>
                 </div>
                 <div className="operate">
-                    <button type="button" class="btn">Share</button>
-                    <div class="dropdown">
-                        <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Automations
-                        </a>
-
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Option 1</a></li>
-                            <li><a class="dropdown-item" href="#">Option 2</a></li>
-                            <li><a class="dropdown-item" href="#">Option 3</a></li>
-                        </ul>
-                    </div>
-
+                    <button type="button" className="btn">Share</button>
                 </div>
             </div>
             <div className="body">
@@ -45,19 +40,23 @@ const Dashboard = () => {
                             <Option />
                         </div>
                     </div>
-                    <button class="btn">Add Task</button>
+                    <button className="btn">Add Task</button>
                 </div>
                 <div className="bodyContent">
                     <div className="contentHeader">
                         <div className='feature'>
-                            <div className="list">
-                                <Todo />
-                                <span>To Do</span>
-                            </div>
-                            <div className="list">
-                                <Form />
-                                <span>Validation Check</span>
-                            </div>
+                            <NavLink style={navLinkStyles} to='/todo'>
+                                <div className="list">
+                                    <Todo />
+                                    <span>To Do</span>
+                                </div>
+                            </NavLink>
+                            <NavLink style={navLinkStyles} to='/validation'>
+                                <div className="list">
+                                    <Form />
+                                    <span>Validation Check</span>
+                                </div>
+                            </NavLink>
                         </div>
                         <div className='searchSection'>
                             <input type='text'
@@ -66,54 +65,11 @@ const Dashboard = () => {
                         </div>
                     </div>
                     <div className="content">
-                        <div className="taskBox">
-                            <div className="taskTitle">
-                                TO DO
-                            </div>
-                            <div className='cardList'>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> Develop Dashboard</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> Develop Dashboard</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. The capital city of Nepal is Kathmandu.Some quick example text to build on the card title and make up the bulk of the card's content. The capital city of Nepal is Kathmandu.Some quick example text to build on the card title and make up the bulk of the card's content. The capital city of Nepal is Kathmandu.Some quick example text to build on the card title and make up the bulk of the card's content. The capital city of Nepal is Kathmandu.Some quick example text to build on the card title and make up the bulk of the card's content. The capital city of Nepal is Kathmandu.Some quick example text to build on the card title and make up the bulk of the card's content. The capital city of Nepal is Kathmandu.</p>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> Develop Dashboard</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> Develop Dashboard</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> Develop Dashboard</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> Develop Dashboard</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                        <Outlet />
                     </div>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
